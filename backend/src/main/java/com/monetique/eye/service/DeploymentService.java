26e12f3d0c3ac65ae3a6641a79c1f926155cb463
YoussefBA10 <youssef.benarous@esprit.tn>
2026-04-14 23:30:16 +0100

    Fixing Integration node deployment

--- backend/src/main/java/com/monetique/eye/service/DeploymentService.java
+++ backend/src/main/java/com/monetique/eye/service/DeploymentService.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @Service
